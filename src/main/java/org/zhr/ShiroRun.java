26f98b48129f2c537624fd7e12c3155418f80e0a
zhanghaoran <2017929959@qq.com>
2022-10-27 09:00:19 +0800

    ""

--- src/main/java/org/zhr/ShiroRun.java
+++ src/main/java/org/zhr/ShiroRun.java
@@ -1,2 +1,34 @@
-package org.zhr;public class ShiroRun {
+package org.zhr;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+
+public class ShiroRun {
+    public static void main(String[] args) {
+        // 初始化获取SecurityManager
+        IniSecurityManagerFactory factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+        SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+        // 获取Subject对象
+        Subject subject = SecurityUtils.getSubject();
+        // 创建token对象，web应用用户名密码从页面传递
+        AuthenticationToken token = new UsernamePasswordToken("zhangsan", "z3");
+        // 完成登录
+        try {
+            subject.login(token);
+            System.out.println("登录成功");
+        } catch (UnknownAccountException e) {
+            e.printStackTrace();
+            System.out.println("用户不存在");
+        } catch (IncorrectCredentialsException e) {
+            e.printStackTrace();
+            System.out.println("密码错误");
+        } catch (AuthenticationException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
